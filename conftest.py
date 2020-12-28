ccca02e72c894112ed549d725fc82ae000baea6a
Irina Nechaeva <ivn@tut.by>
2020-12-28 13:05:13 +0300

    Иправления

--- conftest.py
+++ conftest.py
@@ -24,17 +24,6 @@ def load_config(file):
 
 
 @pytest.fixture
-def app(request):
-    global fixture
-    browser = request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--target"))['web']
-    if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=config['web']['baseUrl'])
-    fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
-    return fixture
-
-
-@pytest.fixture
 def app(request, config):
     global fixture
     browser = request.config.getoption("--browser")
